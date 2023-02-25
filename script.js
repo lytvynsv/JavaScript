ed293ec2b338687b297a16245eadf5fbbb0ac42b
litvinsv <litvin.svtln@gmail.com>
2023-02-25 21:57:43 +0100

    home work 22

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-firstNumber = prompt('Enter first number')
-secondNumber = prompt('Enter second number')
+const firstNumber = prompt('Enter first number')
+const secondNumber = prompt('Enter second number')
+
+if(firstNumber === '' || secondNumber === '') {
+    alert("Oops! Something went wrong.");
+}
+
+if(firstNumber < secondNumber) {
+alert("Are you sure you want to continue?");
+}
+
+alert("Difference is" + " " + (firstNumber - secondNumber));
+
+if(secondNumber > 0) {
+    alert("Quotient is" + " " + (firstNumber / secondNumber));
+} else if (secondNumber == 0) {
+    alert("Undefined");
+}
 
 alert("Sum is" + " " + (Number(firstNumber) + Number(secondNumber)))
-alert("Difference is" + " " + (firstNumber - secondNumber))
 alert("Product is" + " " + (firstNumber * secondNumber))
-alert("Quotient is" + " " + (firstNumber / secondNumber))
