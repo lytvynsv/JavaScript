4d7c529863796300d628d75835cda156b89c3b34
litvinsv <litvin.svtln@gmail.com>
2023-02-25 14:43:14 +0100

    home work for lesson 21

---
+++ script.js
@@ -0,0 +1,7 @@
+firstNumber = prompt('Enter first number')
+secondNumber = prompt('Enter second number')
+
+alert("Sum is" + " " + (Number(firstNumber) + Number(secondNumber)))
+alert("Difference is" + " " + (firstNumber - secondNumber))
+alert("Product is" + " " + (firstNumber * secondNumber))
+alert("Quotient is" + " " + (firstNumber / secondNumber))
