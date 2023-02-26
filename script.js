780d2c124faa48fbb5c43d79f319ff5a5642ba07
litvinsv <litvin.svtln@gmail.com>
2023-02-26 12:51:55 +0100

    home work for lesson 22

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const firstNumber = prompt('Enter first number')
 const secondNumber = prompt('Enter second number')
 
-if(firstNumber === '' || secondNumber === '') {
-    alert("Oops! Something went wrong.");
-}
-
-if(firstNumber < secondNumber) {
-alert("Are you sure you want to continue?");
-}
-
-alert("Difference is" + " " + (firstNumber - secondNumber));
-
-if(secondNumber > 0) {
-    alert("Quotient is" + " " + (firstNumber / secondNumber));
+if ((firstNumber == null || firstNumber == "") || (secondNumber == null || secondNumber == "")) {
+    alert ("Oops! Something went wrong.");
 } else if (secondNumber == 0) {
-    alert("Undefined");
+    alert ("Undefined");
+    alert ("Sum is" + " " + (Number(firstNumber) + Number(secondNumber)));
+    alert ("Product is" + " " + (firstNumber * secondNumber));
+    alert ("Difference is" + " " + (firstNumber - secondNumber));
+} else if (firstNumber < secondNumber) {
+    let desicion = confirm ('Are you sure you want to continue?');
+    if (desicion == true) {
+    alert ("Difference is" + " " + (firstNumber - secondNumber));
+    alert ("Sum is" + " " + (Number(firstNumber) + Number(secondNumber)));
+    alert ("Product is" + " " + (firstNumber * secondNumber));
+    alert ("Quotient is" + " " + (firstNumber / secondNumber));
+} else {
+    alert ("Sum is" + " " + (Number(firstNumber) + Number(secondNumber)));
+    alert ("Product is" + " " + (firstNumber * secondNumber));
+    alert ("Quotient is" + " " + (firstNumber / secondNumber));
+    }
+} else {
+    alert ("Sum is" + " " + (Number(firstNumber) + Number(secondNumber)));
+    alert ("Product is" + " " + (firstNumber * secondNumber));
+    alert ("Difference is" + " " + (firstNumber - secondNumber));
+    alert ("Quotient is" + " " + (firstNumber / secondNumber));
 }
-
-alert("Sum is" + " " + (Number(firstNumber) + Number(secondNumber)))
-alert("Product is" + " " + (firstNumber * secondNumber))
-
